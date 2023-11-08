ff554b1e1d096933d4eed112d557aff32af8c03a
baixiaohui <1974089790@qq.com>
2023-11-08 22:23:16 +0800

    20231108

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,28 @@
-import { defineUserConfig } from 'vuepress'
+import { defaultTheme } from '@vuepress/theme-default'
 
-export default defineUserConfig({
-  lang: 'zh-CN',
-  title: '你好， VuePress ！',
-  description: '这是我的第一个 VuePress 站点',
-})
+export default {
+  theme: defaultTheme({
+    // 在这里进行配置
+    base: '/xiaobaicoding-blogs/',
+    title: '小白Coding日志',
+    description: '这里是小白Coding日志,会以轻松简单的方式,通过分享编程学习日常,帮助大家一步步掌握编程技能!',
+    logo: 'https://vuejs.org/images/logo.png',
+    navbar: [
+      {text: '掘金', link: 'https://juejin.cn/user/515006730085735'},
+      {text: 'Github', link: 'https://github.com/it1998-web'},
+    ],
+    sidebar: [
+      {
+        text: '学习',
+        link: '/introduce/',
+        children: [
+         {
+          text: '01.md',
+          link: '/introduce/01.md',
+         }
+         
+        ]
+      },
+    ]
+  }),
+}
