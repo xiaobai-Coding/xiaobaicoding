a3481e46dfaab8a907f95af5147674bd8cf19754
bxh <bxh>
2023-11-13 13:37:38 +0800

    feat: 增加网站logo和标题

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,15 +1,17 @@
 import { defaultTheme } from '@vuepress/theme-default'
 // 部署执行命令，切换到git命令行工具：./deploy.sh
 module.exports = {
+  // locales站点配置,所有在 Locale 配置 章节内的配置项都可以在 locales 中使用。
+  title: '小白Coding日志',
+  lang: 'zh-CN',
+  description: '这里是小白Coding日志,会以轻松简单的方式,通过分享编程学习日常,帮助大家一步步掌握编程技能!',
+  head: [['link', { rel: 'icon', href: '/images/logo1.png' }]],
   theme: defaultTheme({
-    // 在这里进行配置
-    title: '小白Coding日志',
-    lang: 'zh-CN',
-    description: '这里是小白Coding日志,会以轻松简单的方式,通过分享编程学习日常,帮助大家一步步掌握编程技能!',
-    logo: 'https://vuejs.org/images/logo.png',
+    // 在这里进行配置    
+    logo: '/images/logo1.png',
     navbar: [
       {text: '掘金', link: 'https://juejin.cn/user/515006730085735'},
-      {text: 'Github', link: 'https://github.com/it1998-web'},
+      {text: 'Github', link: 'https://github.com/xiaobai-Coding'},
     ],
     sidebar: [
       {
